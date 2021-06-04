293b56e520b53ca6431f9659413ecd43fa3b0c8d
nozky <norvillie.villaruel@edu.sait.ca>
2021-06-04 14:40:58 -0600

    activated contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,13 +7,13 @@ const Contact = () => {
     
         <h2>Get in touch</h2>
 
-        <form className='contact-form' action="#">
+        <form className='contact-form' action="https://formspree.io/f/meqvgdzj" method='POST'>
           <div>
             <input  type="text" id='name' name='name' placeholder='Name'/>
             <input  type="text" id='subject' name='subject' placeholder='Subject'/>
           </div>
           <div>
-            <input type="email" id='name' name='email' placeholder='Email' />
+            <input type="email" id='name' name='_replyto' placeholder='Email' />
           </div>
           <div>
             <input type="text" id='message' name='message' placeholder='Message'/>
